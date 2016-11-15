17e3d52b2a479ba67993a64e993356bd655da273
carmen-c <carmen.csmwc@gmail.com>
2016-11-15 11:21:44 -0800

    Initial Commit

---
+++ Every.Do/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Every.Do
+//
+//  Created by carmen cheng on 2016-11-15.
+//  Copyright © 2016 carmen cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) NSDate *detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Every.Do/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Every.Do
+//
+//  Created by carmen cheng on 2016-11-15.
+//  Copyright © 2016 carmen cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
